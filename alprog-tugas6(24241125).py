312cfd83433b92fb817aa7ab18d99ca0b21e92c5
Elen jaelani <elenalanelen@gmail.com>
2024-11-17 01:27:45 +0800

    Update alprog-tugas6(24241125).py

--- alprog-tugas6(24241125).py
+++ alprog-tugas6(24241125).py
@@ -1,6 +1,14 @@
+# Modifikasi Tugas 2
+
 x = 25
 y = 24
-luas = x * y * 1/2
-print("nilai x adalah : ", x) 
-print("nilai y adalah : ", y) 
-print("luas adalah:", luas)
+luas = 600
+
+x = input("masukan nilai x:")
+print("nilai x adalah :",x)
+
+y = input("masukan nilai y:")
+print("nilai y adalah :",y)
+
+luas = input("masukan nilai luas:")
+print("nilai luas adalah :",luas)
