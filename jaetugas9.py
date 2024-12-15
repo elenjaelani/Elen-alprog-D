c3ef7ee69d1694978cda53187a9c48a477cc7f07
Elen jaelani <elenalanelen@gmail.com>
2024-12-15 12:07:35 +0800

    Add files via upload

---
+++ jaetugas9.py
@@ -0,0 +1,17 @@
+# suhu dalam celcius = 25
+# suhu dalam fahrenheit = 77.0
+# suhu dalam kelvin = 298.15
+# suhu dalam reamur = 20.0
+
+# suhu dalam celcius
+celcius = 25
+
+# rumus koversi suhu
+farenheit = (celcius * 9/5) + 32
+kelvin = celcius + 273.15
+reamur = celcius * 4/5
+
+# cetak nilai suhu
+print("suhu dalam fahrenheit:", farenheit )
+print("suhu dalam kelvin:", kelvin)
+print("suhu dalam reamur:", reamur)
